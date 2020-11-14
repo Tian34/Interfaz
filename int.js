9a4a57c9b5a03dedcacf7ad8bb37c89424ea64c4
Tian34 <70531781+Tian34@users.noreply.github.com>
2020-11-14 17:30:23 -0500

    Add files via upload

---
+++ int.js
@@ -0,0 +1,424 @@
+var mte=function(){
+
+		var n1=document.getElementById("e");
+		var n2=document.getElementById("est");
+	
+		
+		n2.className="estante";
+
+
+}
+
+var no1=function(){
+
+var n1=document.getElementById("e");
+var n2=document.getElementById("est");
+n2.className="no";
+
+		}
+
+var mts=function(){
+
+		var n1=document.getElementById("s");
+		var n2=document.getElementById("si");
+	
+		
+		n2.className="silla";
+		
+
+}
+
+var no2=function(){
+
+var n1=document.getElementById("s");
+var n2=document.getElementById("si");
+n2.className="no";
+
+		}
+
+var mtp=function(){
+
+		var n1=document.getElementById("p");
+		var n2=document.getElementById("pt");
+	
+		
+		n2.className="patin";
+		
+
+}
+
+var no3=function(){
+
+var n1=document.getElementById("p");
+var n2=document.getElementById("pt");
+n2.className="no";
+
+		}
+
+
+var msig=function(){
+
+		var n1=document.getElementById("ir");
+		var n2=document.getElementById("otra");
+		var n3=document.getElementById("vol");
+		var n4=document.getElementById("fondo");
+
+		n1.className="no";
+		n2.className="siguiente";
+		n3.className="volv";
+		n4.className="no";
+
+}
+
+	
+
+
+var mdp1=function(){
+
+		var n1=document.getElementById("boton1");
+		var n2=document.getElementById("boton2");
+		var n3=document.getElementById("d1");
+		var n4=document.getElementById("d2");
+		
+		n1.className="no";
+		n2.className="bd";
+		n3.className="no";
+		n4.className="dialogo";
+
+}
+
+var mdp2=function(){
+
+		var n1=document.getElementById("boton2");
+		var n2=document.getElementById("boton3");
+		var n3=document.getElementById("d2");
+		var n4=document.getElementById("d3");
+		
+		n1.className="no";
+		n2.className="bd";
+		n3.className="no";
+		n4.className="dialogo";
+
+}
+
+var mdp3=function(){
+
+		var n1=document.getElementById("pp");
+		var n2=document.getElementById("d3");
+		var n3=document.getElementById("nombrepapa");
+		var n4=document.getElementById("boton3");
+
+		n1.className="no";
+		n2.className="no";
+		n3.className="no";
+		n4.className="no";
+
+}
+
+
+
+
+var sonido = new Audio();
+sonido.src="musica.mp3";
+sonido.play();
+
+
+function subir(){
+	sonido.volume += .1; 	
+}
+
+function bajar(){
+	sonido.volume -= .1;
+}
+
+
+
+
+var mte1=function(){
+
+		var n1=document.getElementById("e1");
+		var n2=document.getElementById("est1");
+	
+		
+		n2.className="estante";
+
+
+}
+
+var no11=function(){
+
+var n1=document.getElementById("e1");
+var n2=document.getElementById("est1");
+n2.className="no";
+
+		}
+
+var mts1=function(){
+
+		var n1=document.getElementById("s1");
+		var n2=document.getElementById("si1");
+	
+		
+		n2.className="silla";
+		
+
+}
+
+var no21=function(){
+
+var n1=document.getElementById("s1");
+var n2=document.getElementById("si1");
+n2.className="no";
+
+		}
+
+var mtp1=function(){
+
+		var n1=document.getElementById("p1");
+		var n2=document.getElementById("pt1");
+	
+		
+		n2.className="patin";
+		
+
+}
+
+var no31=function(){
+
+var n1=document.getElementById("p1");
+var n2=document.getElementById("pt1");
+n2.className="no";
+
+		}
+
+
+
+
+
+
+
+var casa=function(){
+
+var n1=document.getElementById("otra");
+var n2=document.getElementById("casa");
+n1.className="no";
+n2.className="fon";
+
+		
+		}
+
+
+var tienda1=function(){
+
+var n1=document.getElementById("otra");
+var n2=document.getElementById("tienda");
+n1.className="no";
+n2.className="sig";
+
+		
+		}
+
+var cai1=function(){
+
+var n1=document.getElementById("otra");
+var n2=document.getElementById("cai");
+n1.className="no";
+n2.className="sig";
+
+		
+		}
+
+var callecai=function(){
+
+var n1=document.getElementById("cai");
+var n2=document.getElementById("otra");
+n1.className="no";
+n2.className="otra";
+
+		
+		}
+
+var calletien=function(){
+
+var n1=document.getElementById("tienda");
+var n2=document.getElementById("otra");
+n1.className="no";
+n2.className="otra";
+
+		
+		}
+
+var texpuerta=function(){
+
+var n1=document.getElementById("puerta");
+var n2=document.getElementById("puertat");
+
+n2.className="silla";
+
+		
+		}
+
+var notexpuerta=function(){
+
+var n1=document.getElementById("puerta");
+var n2=document.getElementById("puertat");
+n2.className="no";
+
+
+		
+		}
+
+var texrejilla=function(){
+
+var n1=document.getElementById("rejilla");
+var n2=document.getElementById("rejillat");
+
+n2.className="patin";
+
+		
+		}
+
+var notexrejilla=function(){
+
+var n1=document.getElementById("rejilla");
+var n2=document.getElementById("rejillat");
+n2.className="no";
+
+
+		
+		}
+
+var texcai=function(){
+
+var n1=document.getElementById("cai1");
+var n2=document.getElementById("cait");
+
+n2.className="estante";
+
+		
+		}
+
+var notexcai=function(){
+
+var n1=document.getElementById("cai1");
+var n2=document.getElementById("cait");
+n2.className="no";
+
+
+		
+		}
+
+
+var robo=function(){
+
+var n1=document.getElementById("tienda");
+var n2=document.getElementById("robo");
+n1.className="no";
+n2.className="otra";
+
+		
+		}
+
+
+
+var Drob1=function(){
+
+var n1=document.getElementById("Drobo1");
+var n2=document.getElementById("Dladron1");
+var n3=document.getElementById("nombreladron");
+var n4=document.getElementById("Dcajero");
+var n5=document.getElementById("nombrecajero");
+var n6=document.getElementById("Drobo2");
+
+n1.className="no";
+n2.className="no";
+n3.className="no";
+n4.className="dialogo";
+n5.className="nombre";
+n6.className="bd";
+
+		}
+
+var Drob2=function(){
+
+var n1=document.getElementById("Drobo2");
+var n2=document.getElementById("Dladron2");
+var n3=document.getElementById("nombreladron");
+var n4=document.getElementById("Dcajero");
+var n5=document.getElementById("nombrecajero");
+var n6=document.getElementById("Drobo3");
+
+n1.className="no";
+n2.className="dialogo";
+n3.className="nombre";
+n4.className="no";
+n5.className="no";
+n6.className="bd";
+
+		}
+
+var Drob3=function(){
+
+var n1=document.getElementById("Drobo3");
+var n2=document.getElementById("nombreladron");
+var n3=document.getElementById("Dladron2");
+var n4=document.getElementById("robo");
+var n5=document.getElementById("tienda1");
+
+
+n1.className="no";
+n2.className="no";
+n3.className="no";
+n4.className="no";
+n5.className="sig";
+
+
+		}
+
+var Dregreso=function(){
+
+var n1=document.getElementById("Dregreso");
+var n2=document.getElementById("nombremiguel");
+var n3=document.getElementById("Bregreso");
+
+
+n1.className="no";
+n2.className="no";
+n3.className="no";
+
+
+
+		}
+
+var robo1=function(){
+
+var n1=document.getElementById("tienda1");
+var n2=document.getElementById("robo");
+
+n1.className="no";
+n2.className="otra";
+
+		
+		}
+
+
+var calletien1=function(){
+
+var n1=document.getElementById("tienda1");
+var n2=document.getElementById("otra");
+n1.className="no";
+n2.className="otra";
+
+		
+		}
+
+var tiendafuera=function(){
+
+var n1=document.getElementById("robo");
+var n2=document.getElementById("tienda1");
+n1.className="no";
+n2.className="sig";
+
+		
+		}
+
